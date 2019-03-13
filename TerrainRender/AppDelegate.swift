d2f480bdc5da9a1e4e1140ee39f5f028a2f9a20f
Eugene Feinberg <ugeook@gmail.com>
2019-03-12 19:31:34 -0700

    Initial Commit

---
+++ TerrainRender/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TerrainRender
+//
+//  Created by Eugene Feinberg on 3/12/19.
+//  Copyright © 2019 Eugene Feinberg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
